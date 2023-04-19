2c507c1e7ea5985b64b2430668270958aa106a59
nottrung <124544758+nottrung@users.noreply.github.com>
2023-04-19 13:37:58 +0700

    Update login.java

--- src/qlks/login.java
+++ src/qlks/login.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package qlikhachsan;
+
+package qlks;
 
 import java.awt.*;
 import javax.swing.*;
 
-public class login extends JFrame{
+public class login extends JFrame {
     login()
     {
         getContentPane().setBackground(Color.PINK);

fdb83e04a5b241a5afaf2814a7afd91ae460b11a
nottrung <124544758+nottrung@users.noreply.github.com>
2023-04-27 19:03:51 +0700

    Update and rename bangDK.java to BangDK.java

--- qlikhachsan/src/qlks/BangDK.java
+++ qlikhachsan/src/qlks/BangDK.java
@@ -4,9 +4,9 @@ package qlks;
 import javax.swing.*;
 import java.awt.*;
 
-public class bangDK extends JFrame{
+public class BangDK extends JFrame{
     
-    bangDK() 
+    BangDK() 
     {
         setBounds(0, 0, 1550, 1000);
         setLayout(null);
@@ -18,28 +18,35 @@ public class bangDK extends JFrame{
         anh3.setBounds(0, 0 , 1550, 1000);
         add(anh3);
         
+        //tao thanh ccu
         JMenuBar tccu = new JMenuBar();
         tccu.setBounds(0, 0, 1550, 35);
         anh3.add(tccu);
         
-        JMenu menu = new JMenu("Quan ly khach san");
-        menu.setForeground(Color.blue);
-        tccu.add(menu);
+        //them qlks vaof tccu
+        JMenu qlks = new JMenu("Quan ly khach san");
+        qlks.setForeground(Color.blue);
+        tccu.add(qlks);
         
+        //them phan le tan vao phan qlks 
         JMenuItem letan =new JMenuItem("Le Tan");
-        menu.add(letan);
+        qlks.add(letan);
         
+        //them phan admin vao tccu
         JMenu admin = new JMenu("admin");
         admin.setForeground(Color.red);
         tccu.add(admin);
         
+        //them phan them nhan vien vao phan admin
         JMenuItem themnhanvien = new JMenuItem("Them nhan vien");
         admin.add(themnhanvien);
         
+        //them phan them phong vao phan admin
         JMenuItem themphong = new JMenuItem("Them phong");
         admin.add(themphong);
         
-        JMenuItem themdieukhien = new JMenuItem("Them dieu khien");
+        //them phan them dk vao phan admin
+        JMenuItem themdieukhien = new JMenuItem("Them tai xe");
         admin.add(themdieukhien);
         
         setVisible(true);
@@ -47,6 +54,6 @@ public class bangDK extends JFrame{
     
     public static void main(String[] args)
     {
-        new bangDK();
+        new BangDK();
     }
 }

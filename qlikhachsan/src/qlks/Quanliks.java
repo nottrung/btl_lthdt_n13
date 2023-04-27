3e8897bf366c72b4c78a73db8af1363745f40b4e
nottrung <124544758+nottrung@users.noreply.github.com>
2023-04-27 19:02:30 +0700

    Update and rename Qlikhachsan.java to Quanliks.java

--- qlikhachsan/src/qlks/Quanliks.java
+++ qlikhachsan/src/qlks/Quanliks.java
@@ -4,23 +4,26 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class Qlikhachsan extends JFrame implements ActionListener {
+public class Quanliks extends JFrame implements ActionListener {
 
-    Qlikhachsan() {
-        setBounds(100, 100, 500, 500);
+    Quanliks() {
+        setBounds(100, 100, 500, 500); //set kich co trang
         setLayout(null);
-
+        
+        //them anh vao trang
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/pic1.jpg"));
         JLabel anh1 = new JLabel(i1);
         anh1.setBounds(0, 0, 500, 500); //toa do, chieu dài, rong
         add(anh1);
 
+        //tao chu
         JLabel nd1 = new JLabel("PHAN MEM QUAN LY KHACH SAN");
         nd1.setBounds(0, 0, 1000, 60);
         nd1.setForeground(Color.BLACK);
         nd1.setFont(new Font("serif", Font.PLAIN, 22));
         anh1.add(nd1);
 
+        //tao nut next
         JButton next = new JButton("Next");
         next.setBounds(300, 400, 150, 50);
         next.setBackground(Color.white);
@@ -33,13 +36,14 @@ public class Qlikhachsan extends JFrame implements ActionListener {
 
     }
 
+    //khi nhan vao next thi chuyen sang trang login
     public void actionPerformed(ActionEvent ae) {
         setVisible(false);
-        new login();
+        new Login();
     }
 
     public static void main(String[] args) {
-        new Qlikhachsan();
+        new Quanliks();
     }
 
 }
